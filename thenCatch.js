613efb98e0ff6478e7f08d1e1638f34946ee0f41
Lisa <peolina@live.com.au>
2021-09-21 12:39:45 +1000

    Update thenCatch.js

--- thenCatch.js
+++ thenCatch.js
@@ -14,11 +14,11 @@ const weather = new Promise((resolve, reject) => {
     
 });
 
-// 1. The promise above is incomplete. On line 14 inside the Promise, create conditions for the promise to resolve or reject.
+// 1. The promise above is incomplete. On line 14 inside the Promise, create conditions for the promise to resolve or reject. The steps for doing so are below (a-c)
 // a) The promise should check if raining is true or false and carry out different code according to the condition (if... else)
 // b) If isRaining is false, the promise should resolve with a value of "The party will be in the park!"
 // c) Else isRaining is true, so the promise should reject with a value of "Oh no! The party will be in my apartment."
 
-// 2. Consume the promise using then() and catch().
-// a) Using weather.then(), take the value passed to .then from the Promise and console log it.
+// 2. Consume the promise using then() and catch(). The steps for doing so are below (a-b)
+// a) Using weather.then(), take the value passed to .then from the Promise and console log it. 
 // b) Using .catch, take the value passed to .catch from the Promise and console log it.
